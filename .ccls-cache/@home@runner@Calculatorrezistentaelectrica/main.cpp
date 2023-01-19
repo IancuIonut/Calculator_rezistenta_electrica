08232c1fd5f7f470d04e1ddef908849ddf74c146
Iancu_Ionut <119786862+IancuIonut@users.noreply.github.com>
2023-01-19 18:13:19 +0000

    work in progress

--- .ccls-cache/@home@runner@Calculatorrezistentaelectrica/main.cpp
+++ .ccls-cache/@home@runner@Calculatorrezistentaelectrica/main.cpp
@@ -6,17 +6,17 @@ int main() {
   float r;
   float i;
   float u(u>=0);
-
-
-
-
-
+  char n;
+  char necunoscute;
+  
   cout << "========================================================" << endl;
   cout << '\t'<<'\t'<<'\t'<<">>>> Legea lui OHM  <<<< I = U/R >>>>" << endl;
   cout << "========================================================" << endl;
 
-  cout << " Alegeti necunoscuta: r,i,u ? " << endl << endl;
-  cout << "Introduceti valorile celor doua variabilele cunoscute: " << endl << endl;
+  cout << "Alegeti necunoscuta: r,i,u ? " << endl << endl;
+  cin >> necunoscute;
+  cout << necunoscute << endl;
+  cout << "Introduceti valorile celor doua variabilele cunoscute: "<< endl << endl;
     
   cout << "Valoarea Tensiunii: ";
   cin >> u;
@@ -26,10 +26,18 @@ int main() {
   cout << "Valoarea Intensitatii: ";
   cin >> i;
   //cout << "amperi" << endl;
+
+  cout << "Valoarea Rezistentei: ";
+  cin >> r;
   
-  i = u/r;
+  
+  //n = {i,r,u};
+
+
+  
+  // i = u/r;
   r = u/i;
-  u = i*r;
+  // u = i*r;
   
   cout << "Valoarea Rezistentei: " <<  r << " ohm" << endl;
   cout << "Nr. Octeti - variabila r: " << sizeof(r) << endl; // lungimea în octeți ocupată de variabila r
---
+++ .ccls-cache/@home@runner@Calculatorrezistentaelectrica/main2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int n;
+cin >> n;
+cout << n;
+
+  switch(n) {
+    case 1:
+      cout << "Rezistenta";
+      break;
+    case 2:
+      cout << "Tensiune";
+      break;
+    case 3:
+      cout << "Curent";
+      break;
+    default:
+       cout << "Looking forward to the Weekend";
+
+   
+   }
+
+  
+}
